bf44429a48e3605ca595b29c69dd004baee4e87d
Juan Pablo Hoyos Lopez <91279639+juanhoyos06@users.noreply.github.com>
2025-03-01 16:24:50 -0500

    Codigo utilizado en el ultimo entrenamiento

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -1,22 +1,22 @@
-# from ultralytics import YOLO
+from ultralytics import YOLO
 
-# model = YOLO("yolov8n.pt")
-# output_dir = "./runs"
+model = YOLO("yolov8n.pt")
+output_dir = "./runs/train"
 
-# model.train(
-#     data="./dataset/data.yaml",
-#     epochs=20, 
-#     batch=4, 
-#     imgsz=320, 
-#     device="cpu",
-#     project=output_dir
-# )
+model.train(
+    data="./dataset/data.yaml",
+    epochs=20, 
+    batch=4, 
+    imgsz=320, 
+    device="cpu",
+    project=output_dir
+)
 
-# print(f"✅ Entrenamiento completado. Modelo guardado en 'runs/train'.")
+print(f"✅ Entrenamiento completado. Modelo guardado en 'runs/train'.")
 
 # Codigo para saber si la ruta existe y obtener la ruta absoluta
-from pathlib import Path
+# from pathlib import Path
 
-dataset_path = Path("./runs/train")
-print(f"El archivo existe: {dataset_path.exists()}")
-print(f"Ruta absoluta: {dataset_path.resolve()}")
+# dataset_path = Path("./runs/train")
+# print(f"El archivo existe: {dataset_path.exists()}")
+# print(f"Ruta absoluta: {dataset_path.resolve()}")
